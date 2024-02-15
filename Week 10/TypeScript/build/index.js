4e72add823a429df7d352bb2a73300dbd5dd5de0
Codeinx <aseyeged@gmail.com>
2024-02-15 13:03:59 +0100

    Another one

--- Week 10/TypeScript/build/index.js
+++ Week 10/TypeScript/build/index.js
@@ -1,3 +1,19 @@
 "use strict";
-const add = (a, b) => a + b;
-console.log(add(3, 2));
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const prompt_sync_1 = __importDefault(require("prompt-sync"));
+const prompt = (0, prompt_sync_1.default)();
+// const add = (a:number, b:number) => a+b;
+// console.log(add(3,2));
+// var test: (string|number|boolean)[] = []
+// var newElement = test.push("Dominion")
+// var newElement = test.push(39)
+// var newElement = test.push(true)
+// console.log(test);
+function conCat(str1, str2) {
+    var str1 = prompt("Enter first word");
+    var str2 = prompt("Enter second word");
+    return str1.concat(" ", str2);
+}
--- Week 10/TypeScript/index.ts
+++ Week 10/TypeScript/index.ts
@@ -1,3 +1,76 @@
-const add = (a:number, b:number) => a+b;
+// import promptSync from 'prompt-sync';
 
-console.log(add(3,2));
+// const prompt = promptSync();
+// const add = (a:number, b:number) => a+b;
+
+// console.log(add(3,2));
+
+// var test: (string|number|boolean)[] = []
+// var newElement = test.push("Dominion")
+// var newElement = test.push(39)
+// var newElement = test.push(true)
+// console.log(test);
+
+// function conCat(str1:string, str2:string):string {
+//     // var str1 = prompt("Enter first word");
+//     // var str2 = prompt("Enter second word");
+//     return str1.concat(" ",str2);
+// }
+
+// console.log(conCat("Dominion", "Aseyege"));
+
+
+// function sumOfArray(arr:number[]): number{
+//     let y:number = 0;
+//     for (let x = 0; x < arr.length; x++) {
+//         y += arr[x];
+//     }
+//     return y
+// }
+// console.log(sumOfArray([10,20,30,40,50]));
+
+// let flashSale = [
+//     {item: "hollandia evap", price: 2},
+//     {item: "chivita", price: 5},
+//     {item: "hollandia yoghurt", price: 10},
+//     {item: "Lexus", price: 1500},
+//     {item: "Nexus", price: 1000}
+// ]
+
+// let cheap = flashSale.filter((q)=>{
+//     return q.price < 1500
+// })
+
+// let cheap = flashSale.map((k)=>{
+//     return k.item
+// })
+
+// console.log(flashSale[0].item);
+
+// let cart = flashSale.reduce((currentTotal, goods)=>{
+//     return currentTotal + goods.price
+// }, 0)
+
+// console.log(cart);
+
+// interface isCar {
+//     oruko: string
+//     year: number
+//     driving: boolean
+//     color?: string
+// }
+
+// const car: isCar = {
+//     oruko: "Corolla",
+//     year: 2022,
+//     driving: true
+// }
+
+const temi = (oruko:string): string => {
+    return (`My name is ${oruko}`)
+};
+const sule = (a:number, b : number, c:(param: string) => string) => {
+    let sum = a+b;
+    return c("teni");
+};
+sule(5,10,temi);
