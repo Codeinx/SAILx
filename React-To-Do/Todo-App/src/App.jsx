4da0fbf81b10e8e5c1619e64770b9800a3f5c39a
Codeinx <aseyeged@gmail.com>
2024-03-22 09:51:36 +0100

    new

--- React-To-Do/Todo-App/src/App.jsx
+++ React-To-Do/Todo-App/src/App.jsx
@@ -1,16 +1,50 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Headers from './components/header'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([])
+  const [input, setInput] = useState("")
+
+  function nameChange(name) {
+    setInput(name)
+  }
+
+  function addToDo() {
+    if (input.length > 0) {
+      setTodo([...todo, input])
+      setInput("")
+    }
+  }
+
+  function edit(index) {
+    let prompt = window.prompt("Edit Todo")
+    let array = [...todo]
+    array.splice(index, 1, prompt)
+    setTodo([...array])
+  }
+
+  function remove(index) {
+    let array = [...todo]
+    array.splice(index, 1 )
+    setToDo([...array])
+  }
 
   return (
     <>
       
       <h1>Just Do It</h1>
       
+      <Headers value={input} change={() => nameChange(event.target.value)} btnFunc={addToDo}></Headers>
+      {
+        todo.map((item, index) => {
+          return(<p>{item} <button onClick={()=>edit(index)}>Edit</button><button onClick={()=>remove(index)}>Delete</button></p>)
+
+        })
+      }
+
     </>
   )
 }
--- React-To-Do/Todo-App/src/components/header.jsx
+++ React-To-Do/Todo-App/src/components/header.jsx
@@ -1,3 +1,13 @@
-function Headers() {
-    
+function Headers(props) {
+    return (
+        <>
+            <div id="left">
+                <label htmlFor="task">Task Name</label>
+                <input type="text" value={props.value} name="task" id="task" onChange={props.change}/>
+                <button type="button" onClick={props.btnFunc}>Add</button>
+            </div>
+        </>
+    )
 }
+
+export default Headers;
